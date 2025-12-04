909c3ed868b29f7c88e986df71aa8438061f1d8a
JJH <jhjung100877@gmail.com>
2025-12-04 20:33:20 +0900

    수정-아직 안 끝남

--- FrontGUI/MyPanel.java
+++ FrontGUI/MyPanel.java
@@ -17,7 +17,7 @@ public class MyPanel extends JPanel implements ActionListener {
     protected JTextField mtf_BorrowerName, mtf_BorrowerEmail, mtf_BookTitle, mtf_BookAuthor, mtf_BookID;
     protected JButton mb_Run, mb_Clear;
     protected JTextArea mta;
-    protected String[] loanORreturn = {"대출", "반납", "이용자 등록", "이용자 삭제", "책 등록", "책 삭제"};
+    protected String[] loanORreturn = {"이용자 등록", "책 등록", "대출가능 책 보기", "대출중인 책 보기", "대출", "책 반납"};
     protected JComboBox mcb_loanORreturn;
     protected String output = "";
     protected int index;
@@ -87,6 +87,17 @@ public class MyPanel extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         LibraryApplication libApp = new LibraryApplication("선문대학교 중앙도서관");
 
+        if(e.getSource().equals(mcb_loanORreturn)){
+            JComboBox cb = (JComboBox)e.getSource();
+            index = cb.getSelectedIndex(); 
+
+            output = loanORreturn[index] + "자 : " + mtf_BorrowerName.getText() + "\n"
+            + loanORreturn[index] + "책 제목 : " + mtf_BookTitle.getText() + "\n"
+            + loanORreturn[index] + "책 저자 : " + mtf_BookAuthor.getText() + "\n"
+            + loanORreturn[index] + "책 등록번호 : " + mtf_BookID.getText() + "\n"
+            + "-------------------------------------------------" + "\n";
+        }
+        
         if (e.getSource().equals(mcb_loanORreturn)) {
             JComboBox cb = (JComboBox) e.getSource();
             index = cb.getSelectedIndex();
